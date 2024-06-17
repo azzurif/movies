271339f51af73a572e4239d87cc7f430d906e22a
azzrf <fairuzizuan019@gmail.com>
2024-06-17 18:48:06 +0700

    decode keyword in search

--- app/search/[keyword]/page.jsx
+++ app/search/[keyword]/page.jsx
@@ -5,24 +5,28 @@ import Header from "@/app/components/header";
 import { getMovies } from "@/app/libs/api";
 import Card from "@/app/components/skeleton/card";
 
-const Page = ({ params: { query } }) => {
+const Page = ({ params: { keyword } }) => {
 	const [search, setSearch] = useState([]);
 	const [loading, setLoading] = useState(true);
 
+	const decodedKeyword = decodeURI(keyword);
+
 	useEffect(() => {
 		const fetchData = async () => {
-			const searchQuery = await getMovies(`search/multi?query=${query}`);
+			const searchQuery = await getMovies(
+				`search/multi?query=${decodedKeyword}`
+			);
 			setSearch(searchQuery);
 			setLoading(false);
 		};
 
 		fetchData();
-	}, [query]);
+	}, [decodedKeyword]);
 
 	return (
 		<div className="w-full">
-			<title>{`Search for ${query}`}</title>
-			<Header title={`Result for: ${query}`} />
+			<title>{`Search for ${decodedKeyword}`}</title>
+			<Header title={`Result for: ${decodedKeyword}`} />
 			{loading ? <Card /> : <CardList data={search} />}
 		</div>
 	);
