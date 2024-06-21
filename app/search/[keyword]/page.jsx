8da1dea21d465336be75d7d473957fd083ab6a22
azzrf <fairuzizuan019@gmail.com>
2024-06-21 13:46:50 +0700

    change to server component

--- app/search/[keyword]/page.jsx
+++ app/search/[keyword]/page.jsx
@@ -1,33 +1,21 @@
-"use client";
-import React, { useEffect, useState } from "react";
 import CardList from "@/app/components/cardList";
 import Header from "@/app/components/header";
 import { getMovies } from "@/app/libs/api";
 import Card from "@/app/components/skeleton/card";
+import { Suspense } from "react";
 
-const Page = ({ params: { keyword } }) => {
-	const [search, setSearch] = useState([]);
-	const [loading, setLoading] = useState(true);
-
+const Page = async ({ params: { keyword } }) => {
 	const decodedKeyword = decodeURI(keyword);
 
-	useEffect(() => {
-		const fetchData = async () => {
-			const searchQuery = await getMovies(
-				`search/multi?query=${decodedKeyword}`
-			);
-			setSearch(searchQuery);
-			setLoading(false);
-		};
-
-		fetchData();
-	}, [decodedKeyword]);
+	const search = await getMovies(`search/multi?query=${decodedKeyword}`);
 
 	return (
 		<div className="w-full">
 			<title>{`Search for ${decodedKeyword}`}</title>
 			<Header title={`Result for: ${decodedKeyword}`} />
-			{loading ? <Card /> : <CardList data={search} />}
+			<Suspense fallback={<Card />}>
+				<CardList data={search} />
+			</Suspense>
 		</div>
 	);
 };
